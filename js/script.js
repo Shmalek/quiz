cfbe7f74f6fcf26bf8804411e9769dd4d1783468
Shahriz Malek <malekshahriz15@gmail.com>
2018-11-28 02:01:06 +0000

    Worked on Javascript: Main Score!

--- js/script.js
+++ js/script.js
@@ -10,5 +10,58 @@ $(document).ready(function() {
         var q3Result = $("#question3").val();
         var totalScore;
     });
+    
+    var score = 0;
+    
+    
+    function mainScore(){
+        
+        if(score >= -5 && score <= -2){
+            return "unhealthy";
+        
+        } else if (score <= -2 && score >= 2){
+            return "avg";
+            
+        } else {
+            return "healthy";
+        }
+        
+    }
+    
+    function Salad_Pizza(response){
+    
+        if(response === "Pizza"){
+          score = score - 2; 
+          
+        } else {
+          score = score + 2;
+          
+        }
+        
+    }
+    
+    function Coke_Water(response){
+        
+        if(response === "Coke"){
+          score = score - 2; 
+          
+        } else {
+          score = score + 2;
+          
+        }
+        
+    }
+    
+    function Candy_Fruit(response){
+        
+        if(response === "Candy"){
+          score = score - 1; 
+          
+        } else {
+          score = score + 2;
+          
+        }
+        
+    }
 
 });
